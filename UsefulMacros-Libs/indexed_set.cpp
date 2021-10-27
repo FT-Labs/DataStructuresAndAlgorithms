b3ea05d091306fd83e5e699ac78966581d9c4d4d
ardaPhysTech <phystecharda@gmail.com>
2021-10-28 00:06:28 +0300

    blank

--- UsefulMacros-Libs/indexed_set.cpp
+++
@@ -1,6 +0,0 @@
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-
-
-typedef tree<int, null_type,std::less<int>, rb_tree_tag,tree_order_statistics_node_update> indexed_set;
